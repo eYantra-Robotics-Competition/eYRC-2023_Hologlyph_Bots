cd4fc7ecad195d2b0d83c7b7cc5952e6cbf2b594
Srivenkateshwar <srivenakteshwar@e-yantra.org>
2023-10-24 13:14:27 +0530

    task1b

---
+++ hb_task1b_ws/src/hb_task_1b/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+import os 
+from glob import glob
+
+package_name = 'hb_task_1b'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+('share/ament_index/resource_index/packages',
+['resource/' + package_name]),
+('share/' + package_name, ['package.xml']),
+(os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+(os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+(os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+(os.path.join('share', package_name, 'config'), glob('config/*')),
+(os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+(os.path.join('share', package_name, 'script'), glob('script/*'))
+],
+
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='santhoshini',
+    maintainer_email='srivenakteshwar@e-yantra.org',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
