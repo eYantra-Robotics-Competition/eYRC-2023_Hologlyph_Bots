4e5ebcea710b929054be90dd1aaf5fbee1011620
15abhishek <15abhishek@github.com>
2023-11-08 15:11:18 +0530

    Updated 2b publisher

--- hb_task2b/hb_task2b/nextGoalPub.py
+++ hb_task2b/hb_task2b/nextGoalPub.py
@@ -1,4 +1,5 @@
 #######################################     DO NOT MODIFY THIS  FILE     ##########################################
+
 import numpy as np
 import matplotlib.pyplot as plt
 from my_robot_interfaces.srv import NextGoal             
@@ -9,6 +10,7 @@ import time
 from my_robot_interfaces.msg import Goal           
 from my_robot_interfaces.msg import Shape           
 
+
 class ServiceNode(Node):
 
     def __init__(self):
@@ -18,9 +20,48 @@ class ServiceNode(Node):
         self.publish_goal_2 = self.create_publisher(Goal, 'hb_bot_2/goal', 10)
         self.publish_goal_3 = self.create_publisher(Goal, 'hb_bot_3/goal', 10)
         
-        self.publish_shape_1  = self.create_publisher(Shape, 'shape_1', 10)
-        self.publish_shape_2  = self.create_publisher(Shape, 'shape_2', 10)
-        self.publish_shape_3  = self.create_publisher(Shape, 'shape_3', 10)
+        self.timer = self.create_timer(5.0, self.publish_shapes)
+
+    def publish_shapes(self):
+
+        tri_side_length = np.random.randint(100, 200)
+        sq_side_length  = np.random.randint(100, 200)
+        dec_side_length = np.random.randint(100, 200)
+
+        tri_theta = np.random.uniform(0, 2*np.pi)
+        sq_theta  = np.random.uniform(0, 2*np.pi)
+        dec_theta = np.random.uniform(0, 2*np.pi)
+
+        shape1_x, shape1_y, shape1_theta  = generate_triangle(tri_side_length, 350, 150, tri_theta, 100)
+        shape2_x, shape2_y, shape2_theta  = generate_square(sq_side_length, 150, 250, sq_theta, 100)
+        shape3_x, shape3_y, shape3_theta  = generate_decagon(dec_side_length, 400, 400, dec_theta, 100)
+
+        msg_bot_1 = Goal()
+        msg_bot_2 = Goal()
+        msg_bot_3 = Goal()
+
+        msg_bot_1.bot_id = 1
+        msg_bot_1.x = shape1_x
+        msg_bot_1.y = shape1_y
+        msg_bot_1.theta = shape1_theta
+
+        msg_bot_2.bot_id = 2
+        msg_bot_2.x = shape2_x
+        msg_bot_2.y = shape2_y
+        msg_bot_2.theta = shape2_theta
+
+        msg_bot_3.bot_id = 3
+        msg_bot_3.x = shape3_x
+        msg_bot_3.y = shape3_y
+        msg_bot_3.theta = shape3_theta
+
+        # msg_shape_info = Shape()
+        # msg_shape_info.shape_dimension = [tri_side_length, sq_side_length, dec_side_length]
+        # msg_shape_info.shape_theta = [tri_theta, sq_theta, dec_theta]
+
+        self.publish_goal_1.publish(msg_bot_1)
+        self.publish_goal_2.publish(msg_bot_2)
+        self.publish_goal_3.publish(msg_bot_3)
 
 
 def generate_decagon(side_length, x_center, y_center, theta, num_points):
@@ -88,69 +129,15 @@ def generate_square(side_length, x_center, y_center, theta, num_points):
     return x.tolist(), y.tolist(), theta
 
 
+
 def main(args=None):
     rclpy.init(args=args)
     service_node = ServiceNode()
 
-    tri_side_length = np.random.randint(50, 101)
-    sq_side_length  = np.random.randint(50, 101)
-    dec_side_length = np.random.randint(50, 101)
-
-    # Generate random rotation angles for the shapes between 0 and 2*pi
-    tri_theta = np.random.uniform(0, 2*np.pi)
-    sq_theta  = np.random.uniform(0, 2*np.pi)
-    dec_theta = np.random.uniform(0, 2*np.pi)
-
-    shape1_x,shape1_y,shape1_theta  = generate_triangle((tri_side_length), 350, 150, (tri_theta), 100)
-    shape2_x,shape2_y,shape2_theta  = generate_square(sq_side_length, 150, 250, sq_theta, 100)
-    shape3_x,shape3_y,shape3_theta  = generate_decagon(dec_side_length, 400, 400, dec_theta, 100)
-    
-    msg_bot_1 = Goal()
-    msg_bot_2 = Goal()
-    msg_bot_3 = Goal()
-
-    msg_shape_1 = Shape()
-    msg_shape_2 = Shape()
-    msg_shape_3 = Shape()
-
-    msg_bot_1.bot_id = 1
-    msg_bot_1.x = shape1_x
-    msg_bot_1.y = shape1_y
-    msg_bot_1.theta = shape1_theta
-
-    msg_bot_2.bot_id = 2
-    msg_bot_2.x = shape2_x
-    msg_bot_2.y = shape2_y
-    msg_bot_2.theta = shape2_theta
-
-    msg_bot_3.bot_id = 3
-    msg_bot_3.x = shape3_x
-    msg_bot_3.y = shape3_y
-    msg_bot_3.theta = shape3_theta
-
-    msg_shape_1.shape_dimension = float(tri_side_length)
-    msg_shape_1.shape_theta     = tri_theta
-
-    msg_shape_2.shape_dimension = float(sq_side_length)
-    msg_shape_2.shape_theta     = sq_theta
-
-    msg_shape_3.shape_dimension = float(dec_side_length)
-    msg_shape_3.shape_theta     = dec_theta
-
-    while rclpy.ok():
-
-        service_node.publish_goal_1.publish(msg_bot_1)    
-        service_node.publish_goal_2.publish(msg_bot_2)    
-        service_node.publish_goal_3.publish(msg_bot_3)    
-    
-        service_node.publish_shape_1.publish(msg_shape_1)
-        service_node.publish_shape_2.publish(msg_shape_2)
-        service_node.publish_shape_3.publish(msg_shape_3)
-        
-        time.sleep(1)
-
+    rclpy.spin(service_node)
     rclpy.shutdown()
-        
+
+
 if __name__ == '__main__':
     main()
 
