5fd70a3e5f0e6755ac90e61553d438ea69ca0cc4
Srivenkateshwar <srivenkateshwar@e-yantra.org>
2023-11-10 13:16:32 +0530

    feat:added task 2a submission launch file
    Signed-off-by: Srivenkateshwar <srivenkateshwar@e-yantra.org>

---
+++ hb_task2a/launch/submission.launch.py
@@ -0,0 +1,12 @@
+import launch 
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch.actions.ExecuteProcess(
+            cmd=['ros2', 'bag', 'record', '/hb_bot_1/left_wheel_force','/hb_bot_1/right_wheel_force','/hb_bot_1/rear_wheel_force','/detected_aruco'],
+            output='screen'
+        )
+    ])
