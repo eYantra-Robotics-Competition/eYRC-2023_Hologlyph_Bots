e20afa402a1aba246a36fac8a047655ecddcb530
Srivenkateshwar <srivenkateshwar@e-yantra.org>
2023-11-10 15:26:00 +0530

    fix:updated task2a submission.launch.py
    Signed-off-by: Srivenkateshwar <srivenkateshwar@e-yantra.org>

--- hb_task2a/launch/submission.launch.py
+++ hb_task2a/launch/submission.launch.py
@@ -6,7 +6,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return launch.LaunchDescription([
         launch.actions.ExecuteProcess(
-            cmd=['ros2', 'bag', 'record', '/hb_bot_1/left_wheel_force','/hb_bot_1/right_wheel_force','/hb_bot_1/rear_wheel_force','/detected_aruco'],
+            cmd=['ros2', 'bag', 'record', '/hb_bot_1/left_wheel_force','/hb_bot_1/right_wheel_force','/hb_bot_1/rear_wheel_force','/detected_aruco','/shape'],
             output='screen'
         )
     ])
